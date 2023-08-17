740dc5ac1d7f1ff616bcc69f008163760c25c900
speedy <86888934+hasham-m-khan@users.noreply.github.com>
2023-08-17 11:56:03 -0500

    updated backend Khatam signup handler

--- includes/rest-api/signup.php
+++ includes/rest-api/signup.php
@@ -1,6 +1,6 @@
 <?php
 
-function kh_rest_api_users_create_handler ($request) {
+function kh_rest_api_current_khatam_singup_handler ($request) {
   require_once(ABSPATH . "/wp-content/plugins/khatam/repositories/khatamUsersRepository.php");
   require_once(ABSPATH . "/wp-content/plugins/khatam/repositories/khatamRepository.php");
   $currentKhatam = getCurrentKhatam();
@@ -92,24 +92,6 @@ function kh_rest_api_users_create_handler ($request) {
   $response['results'] = $results;
   $response['openSlots'] = $remainingJuz - $noOfNamesToAdd;
   $response['status'] = 2;
-  return $response;
-}
-
-function kh_rest_api_users_read_handler ($request) {
-  require_once(ABSPATH . "/wp-content/plugins/khatam/repositories/khatamUsersRepository.php");
-  require_once(ABSPATH . "/wp-content/plugins/khatam/repositories/khatamRepository.php");
-  $currentKhatam = getCurrentKhatam();
-
-  $response['status'] = 2;
-  $response['khatam_id'] = $currentKhatam->id;
-  $response['data'] = array_map(
-    function ($user) {
-      $user->lastName = substr($user->lastName, 0, 2);
-      return $user;
-    }, 
-    getKhatamUserList($currentKhatam->id)
-  );
-
   return $response;
 }
 
